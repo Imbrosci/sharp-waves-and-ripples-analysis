40d453d28088798f605afa6ff7995d2bce1fe5ed
Barbara Imbrosci <42861552+Imbrosci@users.noreply.github.com>
2022-02-07 15:44:56 +0100

    Update abf_files_loader.py

--- data_loader/abf_files_loader.py
+++ data_loader/abf_files_loader.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from neo import io
-
+import numpy as np
 
 def load_abf(filename):
     """
@@ -13,10 +13,10 @@ def load_abf(filename):
     data = io.AxonIO(filename)
     b1 = data.read()[0]
     chan = {}
-    for ch in range(len(b1.segments[0].analogsignals)):
+    for ch in range(b1.segments[0].analogsignals[0].shape[1]):
         signal = []
         for s in range(len(b1.segments)):
-            signal.append(b1.segments[s].analogsignals[0][:, ch])
+            signal.append(np.array(b1.segments[s].analogsignals[0][:, ch]))
             numb = ch + 1
         chan['ch%d'%numb] = signal
     return chan
