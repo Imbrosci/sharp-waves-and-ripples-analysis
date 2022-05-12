0ef0d9b16c8d317ea59f0d31c82e10f9809bc046
Barbara Imbrosci <42861552+Imbrosci@users.noreply.github.com>
2022-05-12 22:28:02 +0200

    Add files via upload

---
+++ data_loader/abf_files_loader.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from neo import io
+import numpy as np
+
+
+def load_abf(filename):
+    """
+    Created on Tue Sep 11 10:52:17 2018
+
+    extract the analog signal(s) from ABF files
+    (generally acquired with clampex)
+
+    @author: imbroscb
+    """
+    data = io.AxonIO(filename)
+    bl = data.read()[0]
+    data_out = {}
+    ch_numb = 0
+    # loop through the analogsignals
+    for i in range(len(bl.segments[0].analogsignals)):
+        # loop through the channels in each analogsignal
+        for ch in range(bl.segments[0].analogsignals[i].shape[1]):
+            signal = []
+            # loop through the segments (sweeps) and add them to the dict
+            for s in range(bl.size['segments']):
+                signal.append(np.array(bl.segments[s].analogsignals[i][:, ch]))
+                # channel = data._axon_info['listADCInfo'][ch_numb]['ADCChNames']
+            ch_numb += 1
+            data_out['Ch{:}'.format(ch_numb)] = signal
+    return data_out
